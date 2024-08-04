10fd1ebad6d725642c1e467d0c758920077e4ef6
SaundaryaDubey <144812526+SaundaryaDubey@users.noreply.github.com>
2024-08-04 13:37:42 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Homepage from "./Pages/Homepage";
+import Chatpage from "./Pages/Chatpage";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" Component={Homepage} exact></Route>
+        <Route path="/chats" Component={Chatpage}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
